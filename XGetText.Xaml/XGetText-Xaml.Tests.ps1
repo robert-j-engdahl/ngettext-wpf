e03f41f1b6185ce926bfdc1b1ae9e42438a9d651
Robert Jørgensgaard Engdahl <robertengdahl@accuratech.dk>
2017-12-29 00:19:01 +0100

    Initial attempt at xgettext that supports markup extensions in XAML
    XGetText.Xaml/XGetText-Xaml.ps1:
      - Does not create header, so output is invalid.
      - Output written to standard-out.
      - Merges identical msgids.

---
+++ XGetText.Xaml/XGetText-Xaml.Tests.ps1
@@ -0,0 +1,48 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "XGetText-Xaml" {
+    Set-Content -Path TestDrive:\TestFile.xaml -Value '<Window x:Class="NGettext.Wpf.Example.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:wpf="clr-namespace:NGettext.Wpf;assembly=NGettext.Wpf"
+        mc:Ignorable="d"
+        Title="{wpf:Gettext NGettext.WPF Example}" Height="350" Width="525">
+    <Grid>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="*" />
+        </Grid.RowDefinitions>
+        <StackPanel Orientation="Horizontal" HorizontalAlignment="Center">
+            <StackPanel.Resources>
+                <Style TargetType="Button">
+                    <Setter Property="Margin" Value="4,0" />
+                    <Setter Property="Width" Value="80" />
+                </Style>
+            </StackPanel.Resources>
+            <Button CommandParameter="en-US" Command="{StaticResource ChangeCultureCommand}"
+                    Content="{wpf:Gettext English}" />
+            <Button CommandParameter="de-DE" Command="{StaticResource ChangeCultureCommand}"
+                    Content="{wpf:Gettext German}" />
+            <Button CommandParameter="da-DK" Command="{StaticResource ChangeCultureCommand}"
+                    Content="{wpf:Gettext Danish}" 
+                    ToolTip="{wpf:Gettext Danish}" />
+        </StackPanel>
+    </Grid>
+</Window>'
+
+    It "Extracts msgids from given XAML file" {
+        XGetText-Xaml TestDrive:\TestFile.xaml -k Gettext  | Should -Match ([regex]::Escape("msgid ""NGettext.WPF Example"""+ [System.Environment]::NewLine +"msgstr """"" + [System.Environment]::NewLine + [System.Environment]::NewLine)) 
+    }
+
+    It "Annotates msgid with filename and line number" {
+        XGetText-Xaml TestDrive:\TestFile.xaml -k Gettext  | Should -Match ([regex]::Escape("#: TestFile.xaml:8" + [System.Environment]::NewLine + "msgid ""NGettext.WPF Example""")) 
+    }
+
+    It "Joins matching msgids" {
+        XGetText-Xaml TestDrive:\TestFile.xaml -k Gettext  | Should -Match ([regex]::Escape("#: TestFile.xaml:26" + [System.Environment]::NewLine + "#: TestFile.xaml:27" + [System.Environment]::NewLine +"msgid ""Danish"""))
+    }
+}
---
+++ XGetText.Xaml/XGetText-Xaml.ps1
@@ -0,0 +1,35 @@
+﻿function XGetText-Xaml {
+    param(
+    [Parameter(Mandatory=$true,
+               Position=0,
+               ParameterSetName="sourceFiles",
+               HelpMessage="XAML files to extract msgids from.")]
+    [ValidateNotNullOrEmpty()]
+    [string[]]$sourceFiles,
+    [Parameter(Mandatory=$true,
+    HelpMessage="Additional keywords that match MarkupExtensions enclosing msgids to be extracted.")]
+    [Alias("k")]
+    [string[]]
+    $Keywords)
+
+  
+
+    $msgids = @{}
+    $sourceFiles | ForEach-Object {
+        Select-String $_ -Pattern $("""{[a-z]?[a-z0-9]*:"+$Keywords[0]+ " ([^}]*)}""") | ForEach-Object {
+            $msgid = $_.Matches.Groups[1].ToString()
+            if (-Not $msgids.ContainsKey($msgid))
+            {
+               $msgids.Add($msgid, @{Locations = New-Object System.Collections.ArrayList})
+            }
+            [void] $msgids[$msgid].Locations.Add('#: ' + $_.Filename + ':' + $_.LineNumber)
+        } 
+    }
+
+    $result = ""
+    $msgids.GetEnumerator() | ForEach-Object {
+        $result = $result + $($_.Value.Locations -join [System.Environment]::NewLine) + [System.Environment]::NewLine + "msgid """ + $_.Key + """" + [System.Environment]::NewLine + "msgstr """"" + [System.Environment]::NewLine + [System.Environment]::NewLine 
+    }
+
+    Write-Output $result.ToString()
+}
