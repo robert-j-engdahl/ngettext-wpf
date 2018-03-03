21e0759470a161df16dcbd2579dc7e4392cb8528
Robert Jørgensgaard Engdahl <robertengdahl@accuratech.dk>
2018-03-03 23:21:57 +0100

    Improve error reporting + support string interpolation in _()

--- NGettext.Wpf.Tests/TranslationTest.cs
+++ NGettext.Wpf.Tests/TranslationTest.cs
@@ -18,5 +18,16 @@ namespace NGettext.Wpf.Tests
             _localizer.Catalog.GetString(Arg.Is(msgId)).Returns(expectedTranslation);
             Assert.Equal(expectedTranslation, Translation._(msgId));
         }
+
+        [Fact]
+        public void Underscore_Allows_String_Interpolation()
+        {
+            _localizer.Catalog.GetString(Arg.Is("foo {0} bar {1} baz"), Arg.Is(0xdead), Arg.Is(0xbeef))
+                .Returns("expected translation");
+
+            var actual = Translation._("foo {0} bar {1} baz", 0xdead, 0xbeef);
+
+            Assert.Equal("expected translation", actual);
+        }
     }
 }
--- NGettext.Wpf/Translation.cs
+++ NGettext.Wpf/Translation.cs
@@ -1,10 +1,14 @@
-﻿namespace NGettext.Wpf
+﻿using System;
+using System.Linq;
+
+namespace NGettext.Wpf
 {
     public static class Translation
     {
-        public static string _(string msgId)
+        public static string _(string msgId, params object[] @params)
         {
-            return Localizer?.Catalog.GetString(msgId) ?? msgId;
+            if (Localizer is null) throw new Exception("Translation.Localizer must be initialized");
+            return @params.Any() ? Localizer.Catalog.GetString(msgId, @params) : Localizer.Catalog.GetString(msgId);
         }
 
         public static ILocalizer Localizer { get; set; }
