145d28e932fecd259f8a222a272ceed79f3fadc1
Robert Jørgensgaard Engdahl <robert.j.engdahl@gmail.com>
2018-06-19 12:14:01 +0200

    Translation.PluralGettext static method.
    This solves issue #12

--- NGettext.Wpf/Translation.cs
+++ NGettext.Wpf/Translation.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
-using System.Reflection;
 
 namespace NGettext.Wpf
 {
@@ -11,7 +11,7 @@ namespace NGettext.Wpf
             if (Localizer is null)
             {
                 Console.Error.WriteLine("NGettext.WPF.Translation.Localizer not set.  Localization is disabled.");
-                return (@params.Any() ? string.Format(msgId, @params) : msgId);
+                return (@params.Any() ? string.Format(CultureInfo.InvariantCulture, msgId, @params) : msgId);
             }
             return @params.Any() ? Localizer.Catalog.GetString(msgId, @params) : Localizer.Catalog.GetString(msgId);
         }
@@ -19,5 +19,18 @@ namespace NGettext.Wpf
         public static ILocalizer Localizer { get; set; }
 
         public static string Noop(string msgId) => msgId;
+
+        public static string PluralGettext(int n, string singularMsgId, string pluralMsgId, params object[] @params)
+        {
+            if (Localizer is null)
+            {
+                Console.Error.WriteLine("NGettext.WPF.Translation.Localizer not set.  Localization is disabled.");
+                return string.Format(CultureInfo.InvariantCulture, n == 1 ? singularMsgId : pluralMsgId, @params);
+            }
+
+            return @params.Any()
+                ? Localizer.Catalog.GetPluralString(singularMsgId, pluralMsgId, n, @params)
+                : Localizer.Catalog.GetPluralString(singularMsgId, pluralMsgId, n);
+        }
     }
 }
