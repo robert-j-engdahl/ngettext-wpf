c90cabbee9f4bec6b4fa8e31f3b263014286a2a2
Robert Jørgensgaard Engdahl <robert.j.engdahl@gmail.com>
2018-10-02 08:17:22 +0200

    Update assembly info

--- NGettext.Wpf/Properties/AssemblyInfo.cs
+++ NGettext.Wpf/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NGettext.Wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Proper internationalizations upport for WPF (via NGettext).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NGettext.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017, 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.21.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
